d716f943d61e29734699026de8e1f2fa03e34d75
alchemistlowkey <alchemistlowkey@gmail.com>
2024-03-22 00:30:57 +0000

    stored procedure advance

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,34 @@
+-- An SQL script that creates a stored procedure
+-- ComputeAverageWeightedScoreForUser that computes
+-- and store the average weighted score for a student.
+-- Requirements:
+-- Procedure ComputeAverageScoreForUser is taking 1 input:
+-- user_id, a users.id value 
+-- (you can assume user_id is linked to an existing users)
+
+-- Create procedure ComputeAverageWeightedScoreForUsers
+DELIMITER $$
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (IN user_id INT)
+BEGIN 
+	DECLARE total_weighted_score FLOAT;
+	DECLARE total_weight INT;
+
+	SELECT SUM(corrections.score * projects.weight), SUM(projects.weight)
+	INTO total_weighted_score, total_weight
+	FROM corrections
+	JOIN projects ON corrections.project_id = projects.id
+	WHERE corrections.user_id = user_id;
+
+	IF total_weight > 0 THEN
+		UPDATE users
+		SET users.average_score = total_weighted_score / total_weight
+		WHERE users.id = user_id;
+	ELSE
+		UPDATE users
+		SET users.average_score = 0
+		WHERE users.id = user_id;
+	END IF;
+END $$
+
+DELIMITER ;
